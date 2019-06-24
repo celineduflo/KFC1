89af6abf157be22fb533dc459121eceb8f3c107b
Celine Duflo <cduflo@hotmail.com>
2019-06-24 22:22:43 +0200

    1st commit

--- KFC1/ViewController.swift
+++ KFC1/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var celsius: UILabel!
+    @IBOutlet weak var kelvin: UILabel!
+    @IBOutlet weak var farenheit: UILabel!
+    @IBOutlet weak var celsiusStatus: UITextField!
+    @IBAction func celsiusInput(_ sender: Any) {
+    }
+    @IBAction func kelvinInput(_ sender: Any) {
+    }
+    @IBAction func farenInput(_ sender: Any) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        celsius.text = celsiusStatus.text
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // K to F   (K - 273.15) * 9/5 + 32
+    // K to C   K - 273.15
+    // F to K   (F - 32) x 5/9 + 273.15
+    // F to C   (F - 32) x 5/9
+    // C to K   C + 273.15
+    // C to F   (C x 9/5) + 32
+    
+
+    
+
+    
+    
+    
 
 }
 
